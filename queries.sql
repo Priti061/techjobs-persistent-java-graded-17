24f5234db61217d0df36130b5426802290974e61
Priti061 <srijanabaral94@gmail.com>
2023-11-07 18:22:41 -0600

    Added code to connect to mySql Workbench, added code to controllers.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,24 @@
 --Part 1
+--List the columns and their data types in the table as a SQL comment.
+SELECT *
+FROM job;
 
 --Part 2
+--write a query to list the names of the employers in St. Louis City.
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
+
 
 --Part 3
+--write the SQL statement to remove the job table.
+DROP table job;
 
 --Part 4
+--write a query to return the names of all skills that are attached to jobs in alphabetical order. If a skill does not have a job listed, it should not be included in the results of this query.
+
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,39 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message= "Location is required.")
+    @Size(max = 100, message = "Location must be under 100 characters.")
+    public String location;
+
+    @OneToMany
+    @JoinColumn(name= "employer_id")
+    private final List<Job> jobs= new ArrayList<>();
+
+    public Employer() {
+
+    }
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
